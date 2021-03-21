b19407b6ebb5a6fab879c4723906facce760791f
Alessandro Malhotra <alessandro244@outlook.com>
2021-03-21 13:47:25 +0000

    Update game.py

--- game.py
+++ game.py
@@ -4,6 +4,7 @@ import random
 
 class Game:
 
+	# Initialzed attributes for the game most importantly the phrases which will be used to play
 	def __init__(self):
 		self.missed = 0
 		self.phrases = [Phrase('life is like a box of chocolates'), Phrase('luke i am your father'), Phrase('live every day like it is your last'), Phrase('a piece of cake'), Phrase('speak of the devil')]
@@ -11,6 +12,7 @@ class Game:
 		self.guesses = [' ']
 
 	
+	# This selects a random phrase from phrases list each time the game is played
 	def get_random_phrase(self):
 		
 		for phrase in self.phrases:
@@ -27,6 +29,7 @@ class Game:
 		print(welcome_message)
 
 
+	# This starts the game with the welcome message and displays number of incorrect guesses
 	def start(self):
 		self.welcome()
 		while (self.missed < 5 and not self.active_phrase.check_complete(self.guesses)):
@@ -40,11 +43,13 @@ class Game:
 		self.game_over()
 
 
+	# Prompts the user for a guess 
 	def get_guess(self):
 		self.guess = input('\n\nEnter a letter: ')
 		return self.guess
 
 
+	# Informs the user if they won or lost 
 	def game_over(self):
 		if self.missed == 5:
 			print('You lost!')
@@ -52,6 +57,7 @@ class Game:
 			print('Congratulations you won!')
 
 
+	# Prompts the user if they would like to play again, if yes the game stats restart along with a different phrase. 
 	def play_again(self):
 		play_again = input("\nGame Over! Would you like to play again Yes/No: ")
 
