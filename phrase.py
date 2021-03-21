5d086ccb561d2b0167fb0b85681de6ac3fa3a51d
Alessandro Malhotra <alessandro244@outlook.com>
2021-03-21 13:50:34 +0000

    Update phrase.py

--- phrase.py
+++ phrase.py
@@ -5,6 +5,8 @@ class Phrase:
 		self.phrase = phrase.lower()
 
 
+	# Displays the letter location in the phrase on screen,
+	# If the user inputs a letter that is in the phrase
 	def display(self, guesses):
 
 		for letter in self.phrase:
@@ -13,14 +15,15 @@ class Phrase:
 			else:
 				print('_', end=' ')
 
-	
+	# Checks if user input is not a letter and asks them to try again
 	def check_guess(self, guess):
 		if len(guess) > 1 or guess.isalpha() == False:
 			print("This is not a valid entry. Try again.")
 			return False
 		return guess in self.phrase
 
-
+	
+	# Checks if user has already gusssed a letter 
 	def check_complete(self, guesses):
 		for letter in self.phrase:
 			if letter not in guesses:
